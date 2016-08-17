7f12225b1044a2366f94363c6219486d0ed2c196
Brett Macy <bam03584@aol.com>
2016-08-17 18:28:14 -0400

    App functioning with API calls

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import List from './List'
 import Input from './Input'
 import logo from './logo.svg'
 
-const TOKEN = 'illustriousvoyage'
+const TOKEN = 'gypsy'
 
 class App extends Component {
 
   constructor () {
     super()
     this.state = {
-      listItems: [
-      ]
+      listItems: []
     }
   }
+  componentWillMount () {
+    fetch(`https://one-list-api.herokuapp.com/items?access_token=${TOKEN}`)
+      .then((resp) => { return resp.json() })
+      .then((data) => {
+        this.setState({ listItems: data })
+      })
+  }
 
-  // add the new list text from Input to the state listItems
   addToList = (newListText) => {
     const newListItems = this.state.listItems
-
-    // REPLACE this:
-    //   newListItems.push({ text: newListText, complete: false })
-    //   this.setState({ listItems: newListItems })
-    //
-    // WITH this:
     fetch(`https://one-list-api.herokuapp.com/items?access_token=${TOKEN}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -44,17 +43,37 @@ class App extends Component {
 
   completeItem = (index) => {
     const newListItems = this.state.listItems
-    newListItems[index].complete = !newListItems[index].complete
-    this.setState({
-      listItems: newListItems
+    const item = newListItems[index]
+    fetch(`https://one-list-api.herokuapp.com/items/${item.id}?access_token=${TOKEN}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        item: {
+          complete: `${!item.complete}`
+        }
+      })
+    })
+    .then((response) => { return response.json() })
+    .then((data) => {
+      newListItems[index] = data
+      this.setState({
+        listItems: newListItems
+      })
     })
   }
 
   removeItem = (index) => {
     const newListItems = this.state.listItems
-    newListItems.splice(index, 1)
-    this.setState({
-      listItems: newListItems
+    const item = newListItems[index]
+    fetch(`https://one-list-api.herokuapp.com/items/${item.id}?access_token=${TOKEN}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(() => {
+      newListItems.splice(index, 1)
+      this.setState({
+        listItems: newListItems
+      })
     })
   }
 
